08e214f64ebcd19da9a6d75d0a6ec0099d4e5942
HunterXMaverick <jfo.silva@yavirac.edu.ec>
2018-07-08 19:54:48 -0500

    medio precedimiento completado

--- JS/pong.js
+++ JS/pong.js
@@ -31,8 +31,8 @@ class Ball extends Rect {
     }
 }
 
-class Player extends Rect{
-    constructor(){
+class Player extends Rect {
+    constructor() {
         super(20, 100);
         this.score = 0;
     }
@@ -44,11 +44,6 @@ class Pong {
         this._context = canvas.getContext('2d');
 
         this.ball = new Ball;
-        this.ball.pos.x = 100;
-        this.ball.pos.y = 50;
-
-        this.ball.vel.x = 100;
-        this.ball.vel.y = 100;
 
         this.players = [
             new Player,
@@ -57,7 +52,7 @@ class Pong {
 
         this.players[0].pos.x = 40;
         this.players[1].pos.x = this._canvas.width - 40;
-        this.players.forEach(player =>{
+        this.players.forEach(player => {
             player.pos.y = this._canvas.height / 2;
         });
 
@@ -70,6 +65,14 @@ class Pong {
             requestAnimationFrame(calback);
         };
         calback();
+
+        this.reset();
+    }
+    collide(player, ball) {
+        if (player.left < ball.right && player.right > ball.left &&
+            player.top < ball.bottom && player.bottom > ball.top) {
+            ball.vel.x = -ball.vel.x;
+        }
     }
     draw() {
         this._context.fillStyle = '#000';
@@ -84,17 +87,29 @@ class Pong {
         this._context.fillRect(rect.left, rect.top,
             rect.size.x, rect.size.y);
     }
+    reset(){
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 50;
+
+        this.ball.vel.x = 100;
+        this.ball.vel.y = 100;
+    }
     update(dt) {
         this.ball.pos.x += this.ball.vel.x * dt;
         this.ball.pos.y += this.ball.vel.y * dt;
 
         if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
-            this.ball.vel.x = -this.ball.vel.x;
+            let playerId = this.ball.vel.x < 0 | 0;
+            this.players[playerId].score++;
+            this.reset();
+            console.log(playerId);
         }
         if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
             this.ball.vel.y = -this.ball.vel.y;
         }
         this.players[1].pos.y = this.ball.pos.y;
+
+        this.players.forEach(player => this.collide(player, this.ball));
 
         this.draw();
     }
