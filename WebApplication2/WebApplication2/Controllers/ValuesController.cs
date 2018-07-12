5407c53f3ad9f46efb5e0b759bd9827ab344b9ec
HunterXMaverick <jfo.silva@yavirac.edu.ec>
2018-07-11 20:59:16 -0500

    webservice y conexion a la basse de datos

---
+++ WebApplication2/WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"value(id)";
+        }
+
+        ////// POST api/<controller>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+
+        //}
+        [HttpPost]
+        public puntajes Index([FromBody]puntajes puntos)
+        {
+            return puntos;
+        }
+        public class puntajes
+        {
+            public string puntosUno { get; set; }
+            public string puntosDos { get; set; }
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/conexionBd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication2
+{
+    public class conexionBd
+    {
+        public void Conexion()
+        {
+            var conn = new MySqlConnection();
+            conn.ConnectionString = "DataBase = Score; = localhost; user id = root; password = root";
+            conn.Open();
+            var adaptador = new MySqlDataAdapter("Select * From Puntaje", conn);
+            DataSet data = new DataSet();
+            adaptador.Fill(data);
+            conn.Close();
+
+        }
+    }
+}
